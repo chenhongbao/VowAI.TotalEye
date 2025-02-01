c7dc75bd6c1e856df7c0787da90bb7a2996ff97b
Mr. Chen <chenhongbao@outlook.com>
2025-02-01 18:10:13 +0800

    暂时先不添加服务器。

--- VowAI.TotalEye.ServerShared/ControlActionName.cs
+++ VowAI.TotalEye.ServerShared/ControlActionName.cs
@@ -2,11 +2,11 @@
 {
     public enum ControlActionName
     {
-        Client_Taskkill,
-        Client_Command,
-        HTTP_Connect_Deny,
-        HTTP_Connect_Ignore,
-        HTTP_Session_Redirect,
-        HTTP_Session_OK,
+        Client_Taskkill /* Kill a task on client. */,
+        Client_Command /* Execute a command on client. */,
+        HTTP_Connect_Deny /* Deny an HTTP connect. */,
+        HTTP_Connect_Ignore /* Don't probe an HTTP session. */,
+        HTTP_Session_Redirect /* Redirect a session to another url. */,
+        HTTP_Session_OK /* Return an HTML content to client instead of the original response. */,
     }
 }
---
+++ VowAI.TotalEye.ServerShared/ControlCondition.cs
@@ -0,0 +1,8 @@
+﻿namespace VowAI.TotalEye.ServerShared
+{
+    public enum ControlCondition
+    {
+        Contain /* Inspected names contain the given filtering words. */,
+        Equal /* Inspected names equal the given filtering words. */,
+    }
+}
