f62ca4437d526692137f977ae5f498d80c484768
Mr. Chen <chenhongbao@outlook.com>
2025-02-01 18:52:06 +0800

    Add standard policy tag names.

---
+++ VowAI.TotalEye.ServerShared/PolicyTagName.cs
@@ -0,0 +1,10 @@
+﻿namespace VowAI.TotalEye.ServerShared
+{
+    public enum PolicyTagName
+    {
+        HTTP_Connect /* Policy tag for probing HTTP connection. */,
+        HTTP_Request /* Policy tag for probing HTTP request.  */,
+        HTTP_Response /* Policy tag for probing HTTP response.  */,
+        LOCAL_COMPUTER /* Policy tag for probing local computer.  */,
+    }
+}
